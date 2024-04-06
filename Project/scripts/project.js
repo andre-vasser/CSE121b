9a0fc042666a6207cf9cabf18f51cea4f70a517d
andre-vasser <andre.vasser@gmail.com>
2024-04-06 15:41:17 -0600

    Project Working

---
+++ Project/scripts/project.js
@@ -0,0 +1,37 @@
+{
+    "HeroesName": "Super hero",
+    "homeTown": "Metro City",
+    "formed": 2016,
+    "secretBase": "Super tower",
+    "active": true,
+    "Heroes": [
+      {
+        "name": "Thor",
+        "age": 1,500,
+        "secretIdentity": "Dr. Donald Blake",
+        "powers": ["Strenght", "Immortal", "Lightning"]
+      },
+      {
+        "name": "Superman",
+        "age": 30,
+        "secretIdentity": "Clark kent",
+        "powers": [
+          "Million ton punch",
+          "Damage resistance",
+          "Superhuman reflexes"
+        ]
+      },
+      {
+        "name": "Saitama",
+        "age": 25,
+        "secretIdentity": "Unknown",
+        "powers": [
+          "strenght",
+          "speed",
+          "stamina",
+          "durability",
+          
+        ]
+      }
+    ]
+  }
