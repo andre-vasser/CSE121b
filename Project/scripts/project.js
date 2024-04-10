50180c5ed86463a48c3d652cf51193a6f0a46f6b
andre-vasser <andre.vasser@gmail.com>
2024-04-09 20:19:04 -0600

    Project Pokemon

--- Project/scripts/project.js
+++ Project/scripts/project.js
@@ -1,52 +1,47 @@
-{
-    "HeroesName": "Super hero",
-    "homeTown": "Metro City",
-    "formed": 2016,
-    "secretBase": "Super tower",
-    "active": true,
-    "members": [
-      {
-        "name": "Thor",
-        "age": 1,500,
-        "secretIdentity": "Dr. Donald Blake",
-        "powers": ["Strenght", "Immortal", "Lightning"]
-      },
-      {
-        "name": "Superman",
-        "age": 30,
-        "secretIdentity": "Clark kent",
-        "powers": [
-          "Million ton punch",
-          "Damage resistance",
-          "Superhuman reflexes"
-        ]
-      },
-      {
-        "name": "Saitama",
-        "age": 25,
-        "secretIdentity": "Unknown",
-        "powers": [
-          "strenght",
-          "speed",
-          "stamina",
-          "durability",
-          
-        ]
-      }
-    ]
-    // Fetch data from an API
-fetch('https://wallpaperaccess.com/one-punch-man-saitama')
-.then(response => response.json())
-.then(data => {
-  // Manipulate the DOM or apply styles based on the fetched data
-  const dataContainer = document.getElementById('data-container');
-  dataContainer.innerHTML = '';
+const dataElement = document.querySelector('#data-container');
+const searchbox = document.querySelector('#search');
 
-  data.forEach(item => {
-    const div = document.createElement('div');
-    div.textContent = item.title;
-    dataContainer.appendChild(div);
-  });
-})
-.catch(error => console.error('Error fetching data:', error));
-  }
+
+const displayPokemon = (pokemon)=> {
+        reset ();
+        let article = document.createElement('article');
+        let h3 = document.createElement('h3');
+        h3.textContent = pokemon.name;
+        let imageElement = document.createElement('img');
+        imageElement.setAttribute("src",pokemon.sprites.front_default);
+        let movesElement = document.createElement("ul");
+        article.appendChild(h3);
+        article.appendChild(imageElement);
+        pokemon.moves.forEach(item=>{
+        let moveElement=document.createElement ("li");
+        moveElement.textContent = item.move.name;
+        movesElement.appendChild (moveElement);
+        });
+        article.appendChild(movesElement);
+        dataElement.appendChild(article);
+ 
+}
+const displaynotFound = ()=>{
+  reset ();
+  let article = document.createElement('article');
+  let h3 = document.createElement('h3');
+  h3.textContent = `We couldnt find ${searchbox.value}`;
+  article.appendChild(h3);
+  dataElement.appendChild(article);
+
+}
+const searchPokemon = async () =>{
+  let searchTerm = searchbox.value;
+  const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${searchTerm}`);
+      if (response.ok){
+          const data = await response.json();
+          pokemonData = data;
+          displayPokemon(pokemonData);
+      }else{
+        displaynotFound();
+      }
+}
+
+const reset = () => {
+  dataElement.innerHTML = '';
+}
